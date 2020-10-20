9c75b9bfd19671e12574a768dacddbb09ca2d7c8
Gundulin <cheesit@gmail.com>
2020-10-20 10:58:05 -0600

    mystic vale companion works, but ain't pretty

--- src/app/mystic-vale-companion/mystic-vale-companion.component.ts
+++ src/app/mystic-vale-companion/mystic-vale-companion.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface GameToken {
+  id: string;
+  quantity: number;
+}
+
 @Component({
   selector: 'app-mystic-vale-companion',
   templateUrl: './mystic-vale-companion.component.html',
@@ -7,19 +12,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MysticValeCompanionComponent implements OnInit {
 
-  corruption: number = 0;
+  corruption = {
+    id: 'corruption',
+    quantity: 0
+  };
+  
+  mana = {
+    id: 'mana',
+    quantity: 0
+  };;
+  animalSpirit = {
+    id: 'animalSpirit',
+    quantity: 0
+  };
+  forestSpirit= {
+    id: 'forestSpirit',
+    quantity: 0
+  };
+  skySpirit= {
+    id: 'skySpirit',
+    quantity: 0
+  };
+  wildSpirit= {
+    id: 'wildSpirit',
+    quantity: 0
+  };
+  victoryPoint= {
+    id: 'victoryPoint',
+    quantity: 0
+  };
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+
   }
 
-  subtractCorruption(): void {
-    this.corruption--;
+  decreaseValue(token: GameToken): void {
+    if (token.quantity < 1) {
+      if (token.id === 'corruption') {
+        token.quantity--;
+        return;
+      }
+      else {
+        return;
+      }
+    }
+    token.quantity--;
   }
 
-  addCorruption(): void {
-    this.corruption++;
+  increaseValue(GameToken: GameToken): void {
+    GameToken.quantity++;
+  }
+
+  resetGameTokenQuantities(): void {
+    this.corruption.quantity = 0;
+    this.mana.quantity = 0;
+    this.animalSpirit.quantity = 0;
+    this.forestSpirit.quantity = 0;
+    this.skySpirit.quantity = 0;
+    this.wildSpirit.quantity = 0;
+    this.victoryPoint.quantity = 0;
   }
 
 }
